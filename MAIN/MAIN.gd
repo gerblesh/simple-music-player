1325cb5dd81a562b1aa4a9e19e64f2b7f4aae553
gerblesh <bobflying221@gmail.com>
2022-05-18 16:39:13 -0700

    added new font and theme, as well as more functionality

--- MAIN/MAIN.gd
+++ MAIN/MAIN.gd
@@ -7,10 +7,13 @@ var audio_stream_player = AudioStreamPlayer.new()
 # music directory and song variables
 onready var music_dir_path = OS.get_system_dir(OS.SYSTEM_DIR_MUSIC)
 var current_song = 0
+var song_length
+var song_position
 var music_dir
 var music = []
+var song_names = []
 func _ready():
-	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame") # waiting for the UI ready function to connect it's signals
 	add_child(audio_stream_player)
 	audio_stream_player.connect("finished",self,"next_song")
 	audio_stream_player.pause_mode = Node.PAUSE_MODE_STOP
@@ -19,21 +22,25 @@ func _ready():
 
 func next_song(next = 1):
 	current_song = wrapi(current_song + next,0,music.size()) # constrains the index from getting outside of the array
-	var file_path = music_dir_path+"/"+music[current_song]
+	var file_path = music[current_song]
 	audio_stream_player.stream = AudioLoader.new().loadfile(file_path)
 	audio_stream_player.play()
-
+func is_audio_file(stringname):
+	return stringname.ends_with(".wav") or stringname.ends_with(".ogg") or stringname.ends_with(".mp3")
 func refresh_dir():
 	music_dir = Directory.new()
+	music = []
+	song_names = []
 	if music_dir.open(music_dir_path) == OK: # checking if the file can be opened
 		music_dir.list_dir_begin()
-		var name = music_dir.get_next()
+		var filepath = music_dir.get_next()
 		while true:
-			name = music_dir.get_next()
-			if !music_dir.current_is_dir():
-				if name == "":
-					emit_signal("refresh_dir")
-					return
-				music.append(name)  # adding the file name to the array
+			filepath = music_dir.get_next()
+			if filepath == "":
+				emit_signal("refresh_dir")
+				return
+			if !music_dir.current_is_dir() and is_audio_file(filepath):
+				music.append(music_dir_path+"/"+filepath)  # storing the file path
+				song_names.append(filepath) # storing the file name
 	else:
 		print("failed to open file")
--- MAIN/UI Controls.gd
+++ MAIN/UI Controls.gd
@@ -10,35 +10,56 @@ onready var forward = $controls/buttons/forward
 onready var backward = $controls/buttons/backward
 onready var song_container = $"songs/song container"
 onready var song_label = $controls/name
+onready var time_label = $"controls/time label"
 
 func _ready():
 	scrubber.connect("value_changed",self,"scrub")
 	pause.connect("toggled",self,"play_toggled")
 	connect("refresh_dir",self,"refresh_songs")
 	connect("refresh_dir",self,"refresh_songs")
+	forward.connect("pressed",self,"song_forward")
+	backward.connect("pressed",self,"song_backward")
+	
 
 func play_toggled(button_pressed):
 	get_tree().paused = button_pressed
 
 func _process(_delta):
-	scrubber.max_value = audio_stream_player.stream.get_length()
-	scrubber.value = audio_stream_player.get_playback_position()
-	song_label.text = music[current_song]
+	song_length = audio_stream_player.stream.get_length()
+	song_position = audio_stream_player.get_playback_position()
+	scrubber.max_value = song_length
+	scrubber.value = song_position
+	song_label.text = song_names[current_song]
+	time_label.text = str(secs_to_mins(song_position)," / ",secs_to_mins(song_length))
+
+func secs_to_mins(seconds:float):
+	seconds = round(seconds)
+	var minutes = floor(seconds/60)
+	var new_seconds = fmod(seconds,60)
+	if str(new_seconds).length() < 2:
+		new_seconds = str("0",new_seconds)
+	return str(minutes,":",new_seconds)
 
 func scrub(value):
-	if abs(value - audio_stream_player.get_playback_position()) < 1:
+	if abs(value - audio_stream_player.get_playback_position()) <= 0.01:
 		return
 	audio_stream_player.seek(value)
+
+func song_backward():
+	next_song(-1)
+func song_forward():
+	next_song(1)
 
 func refresh_songs():
 	for I in song_container.get_children():
 		I.queue_free()
-	for I in music:
+	for I in song_names:
 		var button_inst = button_pre.instance()
 		button_inst.name = I
 		button_inst.text = I
 		button_inst.connect("button_pressed",self,"song_pressed")
 		song_container.add_child(button_inst)
+
 func song_pressed(button):
 	current_song = button.get_position_in_parent()
 	next_song(0)
